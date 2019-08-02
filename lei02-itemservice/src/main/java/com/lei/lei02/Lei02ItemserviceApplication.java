29f43d73398220028a60cd8c3a7ab721d42f0944
Administrator <Administrator@tarena>
2019-08-02 10:50:27 +0800

    lei2

---
+++ lei02-itemservice/src/main/java/com/lei/lei02/Lei02ItemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lei.lei02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lei02ItemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lei02ItemserviceApplication.class, args);
+	}
+
+}
